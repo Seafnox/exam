1ac375c63b5cdc5a9f30b04937d0a1ef51fa4a3d
Seafnox <kirill9990@gmail.com>
2015-02-24 04:15:59 +0300

    fix gui not at all
    i'll need set order to update for all frame or only set order to resize
    frame

--- src/main/scala/ui/GUI.scala
+++ src/main/scala/ui/GUI.scala
@@ -1,9 +1,14 @@
 package ui
 
+import java.util
+import javax.swing.table.DefaultTableModel
+
 import akka.actor.ActorRef
 import base._
+import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.swing._
+import scala.swing.event.ValueChanged
 
 /**
  * Created by Кирилл on 20.02.2015.
@@ -15,18 +20,29 @@ class GUI(keeper: ActorRef) extends MainFrame {
   val box = new BoxPanel(Orientation.Vertical)
   val sliderLabelText:String = "Ползунок отвечает за задержку между пакетами сообщений в потоке. Текущая задержка "
   val sliderLabel:Label = new Label(sliderLabelText + "100 миллесекунд")
-  box.contents += sliderLabel
-  box.contents += new Slider {
+  box.contents += new FlowPanel {
+    contents += sliderLabel
+  }
+  // ползунок, для выбора задержки, между пакетами сообщений
+  val slider: Slider = new Slider {
     min = 10
     max = 100
     value = 100
     labels = Map(0 -> new Label("0"),50 -> new Label("50"),100 -> new Label("100"))
     paintLabels = true
-
   }
-  var nodeLabelsWrapper = new BoxPanel(Orientation.Vertical)
-  box.contents += nodeLabelsWrapper
-  box.contents += new Label(" ")
+  box.contents += slider
+  // таблица, в которой будут выводиться результаты
+  var nodeTable = new Table() {
+    rowHeight = 25
+    showGrid = true
+    ignoreRepaint = false
+    val dtm: DefaultTableModel = new DefaultTableModel(null, Array[Any]("Column 1","Column 2").asInstanceOf[Array[Object]])
+    dtm.setRowCount(2)
+    model = dtm
+  }
+  box.contents += nodeTable
+  // кнопки для работы с нодами (добавить и удалить)
   box.contents += new BoxPanel(Orientation.Horizontal) {
     contents += Button("Add Node") { addNode() }
     contents += Swing.HGlue
@@ -34,26 +50,52 @@ class GUI(keeper: ActorRef) extends MainFrame {
   }
   contents = box
 
+  // просим приложение слушать слайдер
+  listenTo(slider)
+
+  reactions += {
+    case ValueChanged(`slider`) =>
+      println("Your slider is now: " + slider.value)
+      if (slider.value <= 100 && slider.value >= 10) {
+        // отсылаем данные о изменениях скорости ползунка
+        keeper.tell(SetDelay(slider.value), null)
+      }
+      sliderLabel.text = sliderLabelText + slider.value + " миллесекунд"
+  }
+
   def addNode(): Unit = {
 
     println("Push CreateNode")
+    // отсылаем команду на создание ноды
     keeper.tell(CreateNode(),null)
   }
 
   def removeNode(): Unit = {
 
     println("Push DestroyNode")
+    // отсылаем команду на удаление ноды
     keeper.tell(DestroyNode(),null)
   }
 
+  // функция перерисовки данных таблицы
   def updateCounters(counters: mutable.Map[ActorRef, Int]): Unit = {
-    nodeLabelsWrapper.contents.clear()
+    val cells = counters.size
+    //println("Cells = " + cells)
+    nodeTable.dtm.setRowCount(cells)
+    nodeTable.model = nodeTable.dtm
+    // двойной репейнт по идее не нужен
+    nodeTable.repaint()
+    var inc = 0
     counters.foreach{counter =>
-      nodeLabelsWrapper.contents += new Label("Node Unknown" + " - " + counter._2)
+      //println("update row " + inc)
+      nodeTable.update( inc, 0, counter._1)
+      nodeTable.update(inc, 1, counter._2)
+      inc+=1
     }
-    nodeLabelsWrapper.repaint()
+    nodeTable.repaint()
   }
 
+  // операция закрытия приложения, вызывает самоубийство кипера
   override def closeOperation() {
     keeper.tell(SetClose(), null)
     sys.exit(0)
